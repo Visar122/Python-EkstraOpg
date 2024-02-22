3f7226836ab6cbf143e89780aa82ceb93f966688
Visar122 <103035889+Visar122@users.noreply.github.com>
2024-02-22 12:13:57 +0100

    Update Teacher.py

--- Teacher.py
+++ Teacher.py
@@ -3,7 +3,7 @@ from Person import Person
 class Teacher(Person):
     def __init__(self, name, lastname, subjects):
         super().__init__(name, lastname)
-        self.subjects = subjects if subjects else []  # Initialize subjects as an empty list if not provided
+        self.subjects = subjects if subjects else []  
         
     def display_info(self):
         super().display_info()
